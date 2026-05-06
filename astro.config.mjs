a4425cf10f2c0e831a086be72b1672477aad007c
Jacob Hall <email@jacobhall.net>
2026-05-06 07:00:27 -0400

    fixes to base URL, trailing slashes in links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://pedestrian-geospatial.github.io',
+  base: '/six-north-sixth/',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()],
   },
