4b1bc2dccac28d134159348fd93ba44b1a0095af
Jacob Hall <email@jacobhall.net>
2026-06-30 09:49:12 -0400

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://pedestrian-geospatial.github.io',
-  trailingSlash: 'always',
+  site: "https://6north6thstreet.com/",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
